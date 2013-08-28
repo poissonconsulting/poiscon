6816b63978491c0dbaf62190969961e0d8c702c3
Joe Thorley <joe@poissonconsulting.ca>
2013-08-28 13:08:51 -0700

    added load_plot function for reading in plot csv files

--- R/load.r
+++ R/load.r
@@ -18,3 +18,27 @@ load_analysis <- function (name = 'analysis') {
   return (invisible (NULL))
 }
 
+#' @title Load plot
+#'
+#' @description 
+#' In current form reads in plot .csv file from current plot folder.
+#' 
+#' @param name the name of the plot (without extension).
+#' @param type the plot type i.e. figures (default) or analysis, slides etc.
+#' @param ext the extension of the file type to read in i.e. ".rds" the 
+#' default or ".csv" the only option currently implemented.
+#' @return a data.frame object
+#' @references 
+#' @export
+load_plot <- function (name='plot', type='figures', ext = "rds") {
+
+  if(!identical(ext,"csv"))
+    stop("ext argument currently only accepts \"csv\"")
+  
+  file <- paste0(get_plots_folder(type = type), '/', name, '.csv')
+  if (file.exists (file))
+    return (read.csv(file))
+  warning (paste0("file ",file," does not exist"))
+  return (invisible (NULL))
+}
+
