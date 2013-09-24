851f16fe0165b885a90055f749e8986712e1942c
Joe Thorley <joe@poissonconsulting.ca>
2013-09-24 15:51:29 -0700

    UTC8 not returns timezone as UTC

--- R/datetime.r
+++ R/datetime.r
@@ -1,13 +1,12 @@
 #
 
 # converts all POSIX datetimes to UTC-0800
-# indicates status using timezone GMT
 UTC8<-function (d)
 {
   gmt<-tz(d)=='GMT'
   d<-with_tz(d,'UTC')
   if(any(!gmt))
     d[!gmt]<-d[!gmt]-hours(8)
-  d<-force_tz(d,'GMT')
+  d<-force_tz(d,'UTC')
   return (d)
 }
