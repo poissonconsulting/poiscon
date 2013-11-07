ad1906a4c30b11a111e196768b2b3fcb92576e62
Joe Thorley <joe@poissonconsulting.ca>
2013-11-07 14:02:15 -0800

    changed save_tables.jags_analysis so that model_number = 1 by default and can be set in function call

--- R/save-tables.r
+++ R/save-tables.r
@@ -6,11 +6,11 @@ save_tables<- function (object, ...) {
 #' @S3method save_tables jags_analysis
 #' @method save_tables jags_analysis
 #' @export
-save_tables.jags_analysis <- function (object) {
+save_tables.jags_analysis <- function (object, model_number = 1) {
   
-  save_table(rhat(subset_jags(object, model_number = 0), combine = FALSE),
+  save_table(rhat(subset_jags(object, model_number = model_number), combine = FALSE),
              'convergence',type='analyses', row.names = T)
 
-  save_table(coef(subset_jags(object, model_number = 0)),'estimates', 
+  save_table(coef(subset_jags(object, model_number = model_number)),'estimates', 
              row.names = T)
 }
