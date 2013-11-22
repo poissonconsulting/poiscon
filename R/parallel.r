c4291d7fd5c2f9b6c108af2eb04dbf4f83ffba93
Joe Thorley <joe@poissonconsulting.ca>
2013-11-22 14:08:54 -0600

    auto

--- R/parallel.r
+++ R/parallel.r
@@ -1,7 +1,7 @@
 
 
 parallel <- function (processes = 3) {
-  if (.Platform$OS.type == ) {
+  if (.Platform$OS.type == "unix") {
     doMC::registerDoMC(3)    
   } else {
     cl <- doSNOW::makeCluster(3)
