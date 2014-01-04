743ba1eb201e63fdc2411924700ff851f6bc2d58
Joe Thorley <joe@poissonconsulting.ca>
2014-01-04 14:35:09 -0600

    added knit functions

---
+++ R/knit-models.r
@@ -0,0 +1,85 @@
+#' @title Knit models
+#'
+#' @description
+#' Takes all models in r files in working directory 
+#' with 'models-' in name and prints model code in markdown
+#' format together with a table of a description of the parameters.
+#' 
+#' @param replacement an optional named character vector which specifies replacement
+#' names for directories as well as the order to print the estimates.
+#' @return Knits model code and parameter descriptions.
+#' @export
+knit_models <- function (replacement) {
+  
+  files <- list.files(pattern = "^models-.*[.][rR]$")
+  
+  titles <- substr(files, 8, 50)
+  titles <- substr(titles, 1, nchar(titles) - 2)
+  
+  if(!is.null(replacement)) {
+    order <- format(1:length(replacement), width = 4)
+    names(order) <- names(replacement)
+    
+    sort <- titles
+    
+    for(i in seq_along(titles)) {
+      for (j in seq_along(order)) {
+        sort[i] <- str_replace_all(sort[i], names(order)[j], order[j])
+      }
+    }
+    files <- files[order(sort)]
+    titles <- titles[order(sort)]
+  }
+  
+  for (i in seq_along(replacement))
+    titles <- str_replace_all(titles, names(replacement)[i], replacement[i])
+  
+  titles <- gsub("(^|[[:space:]|-])([[:alpha:]])", "\\1\\U\\2", titles,
+                 perl=TRUE)
+  
+  kdescription <- function (description) {
+    desc <- description
+    desc <- desc[order(names(desc))]
+    mat <- matrix(NA, nrow = length(desc), ncol = 2)
+    mat[,1] <- names(desc)
+    mat[,2] <- desc
+    colnames(mat) <- c("Variable/Parameter","Description")
+    kable(mat)
+  }
+  
+  for (i in seq_along(files)) {
+    
+    file <- files[i]
+    title <- titles[i]
+    
+    cat(c("\n###"," ",title,"\n"))
+    
+    rm(description)
+    source(file)
+    
+    if (!exists("description")) {
+      description <- NULL
+    } else {
+      kdescription(description)
+    }
+    
+    if (!exists("models")) {
+      models <- NULL
+    } else {
+      
+      for (i in 1:nmodels(models)) {
+        cat(c("\n#### ",title," - Model",i,"\n"))
+        
+        cat("\n\n")
+        
+        cat("```\n")
+        
+        cat(model_code(models)) 
+        
+        cat("\n```\n")
+      }     
+    }
+    
+    invisible ()
+  }
+}
---
+++ R/knit-parameters.r
@@ -0,0 +1,68 @@
+#' @title Knit parameters
+#'
+#' @description
+#' Takes all parameter estimates in tables folder and prints in markdown
+#' format using the knitr::kable function.
+#' 
+#' @param replacement an optional named character vector which specifies replacement
+#' names for directories as well as the order to print the estimates.
+#' @return Prints parameter estimates in markdown format.
+#' @export
+knit_parameters <- function (replacement = NULL) {
+  
+  assert_that(is.null(replacement) || 
+                (is.character(replacement) && is_named(replacement)))
+  
+  dir <- get_tables_folder(type = "results")
+  files <- list.files(dir, pattern = "[.]csv", recursive = TRUE)
+  files <- substr(files,1,nchar(files)-4)
+  files <- files[substr(files,nchar(files)-8, nchar(files)) == "estimates"]
+  
+  if(!is.null(replacement)) {
+    order <- format(1:length(replacement), width = 4)
+    names(order) <- names(replacement)
+    sort <- files
+    
+    for(i in seq_along(files)) {
+      for (j in seq_along(order)) {
+        sort[i] <- str_replace_all(sort[i], names(order)[j], order[j])
+      }
+    }
+    files <- files[order(sort)]  
+  }
+  
+  previous_title <- NULL
+  
+  for (file in files) {
+    
+    csv <- read.csv(file=paste0(dir,"/",file,".csv"))
+    
+    colnames(csv)[1] <- "Parameter"
+    csv <- csv[csv$Parameter != "deviance",]
+    csv <- csv[!is.na(csv$error),]
+    csv <- csv[order(csv$Parameter),]
+    colnames(csv) <- gsub("(^|[[:space:]])([[:alpha:]])", "\\1\\U\\2", colnames(csv),
+                          perl=TRUE)
+    colnames(csv) <- str_replace(colnames(csv), "Sd", "SD")
+    
+    text <- strsplit(file,"/",fixed = T)[[1]]
+    text <- text[-length(text)]
+    title <- paste0(text, collapse = " - ")
+    
+    for (i in seq_along(replacement)) {
+      title <- str_replace_all(title, names(replacement)[i], replacement[i])
+    }
+    
+    title <- gsub("(^|[[:space:]|-])([[:alpha:]])", "\\1\\U\\2", title,
+                  perl=TRUE)
+    
+    if(is.null(previous_title) || title != previous_title) {
+      cat(c("\n###"," ",title,"\n"))
+    }
+    previous_title <- title
+    
+    kable(csv, row.names = FALSE)
+    
+    invisible ()
+  }
+}
