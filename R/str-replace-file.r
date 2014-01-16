0af1c4150a139908e56e5533e3a4feb1bdd00b10
Joe Thorley <joe@poissonconsulting.ca>
2014-01-16 12:05:20 -0600

    added to = NULL arg

--- R/str-replace-file.r
+++ R/str-replace-file.r
@@ -1,10 +1,11 @@
-str_replace_file <- function (file, pattern, replacement) {
+str_replace_file <- function (file, pattern, replacement, to = NULL) {
   
   assert_that(is.string(file))
   assert_that(file.exists(file))
+  assert_that(is.null(to) || is.string(to))
   
   text <- readLines(file)
   text <- str_replace_all(text, pattern, replacement)
-  writeLines(text, file)
-  return (invisible())
+  
+  return (writeLines(text, ifelse(is.null(to), file, to)))
 }
