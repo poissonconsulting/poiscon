ad99fd383d8788b2e8c5153d33e4667dc8903f68
Joe Thorley <joe@poissonconsulting.ca>
2014-01-16 12:05:48 -0600

    updating

--- R/knit-report.r
+++ R/knit-report.r
@@ -11,19 +11,28 @@ knit_report <- function (file = "report.rmd") {
   assert_that(is.string(file))
   assert_that(file.exists(file))
   
-  to <- paste0("output/report/", project_folder(), ".md")
+  to <- paste0("output/report/", project_folder(), "/report.md")
   
-  .figures <- FALSE
+  if(!file.exists(str_replace(to, "/report.md", "")))
+    dir.create(str_replace(to, "/report.md", ""), FALSE, recursive = TRUE)
+  
   knit(file, output = to)
-  str_replace_file(to, "&", "and")
-  # need to replace .md with \.docx for pandoc
-  system(paste0("pandoc -s ", to," -o ",output_dir,".docx"))
+  
+  from <- to
+  to <- str_replace(to, "report.md", "temporary.md")
+    
+  rm_blank_lines_top(from)
+  str_replace_file(from, "&", "and")
+  
+  str_replace_file(from, 
+                   "[#][#].[B][a][c][k][g][r][o][u][n][d]", 
+                   paste0("#### ", Sys.Date(), "\n## Background"), to = to)
+  
+  markdownToHTML(to, str_replace(from, "*[.][m][d]$", ".html"))
+  
+  system(paste0("pandoc -s ", to," -o ", str_replace(from,"[.][m][d]$",".docx")))
 
-  .figures <- TRUE
-  knit(file, output = to)
-  str_replace_file(to, "&", "and")
-  # need to shunt up - delete blank lines...
-#  text <- str_replace_all(text[-(1:2)], "&", "and")
+  file.remove(to)
 
   return (invisible())
 }
