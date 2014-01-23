198c46015051ecbc4a6d7c806df1c6811cd8bb75
Joe Thorley <joe@poissonconsulting.ca>
2014-01-23 12:03:17 -0600

    extract-datetime now standardises

--- R/extract-datetime.r
+++ R/extract-datetime.r
@@ -5,25 +5,44 @@
 #' @param suffix string
 #' @param expand character vector of column names where [prefix]expand[suffix]
 #' and expand must only be Year, Month, Day, Hour, Minute and/or Second
-#' @param tz string of time zone
+#' @param tz string of time zone. Typical values are 
+#' "PST8PDT" if Pacific time i.e., includes daylight savings, 
+#' "PST" if Pacific standard time i.e. no daylight savings
+#' "PDT" if Pacific dayight time i.e. all daylight savings
+#' and "UTC" all in  Universal Coordinated Time (GMT).
+#' "PST8PDT" occurs when times are read from a watch,
+#' "PST" occurs when loggers are deployed in pacific standard time (recommended)
+#' "PDT" occurs when loggers are deployed in pacific daylight time without
+#' adjusting and "UTC" occurs with non-adjusted Vemco receivers.
+#' @param standardised_offset an integer scalar indicating the time difference
+#' in hours between UTC and the desired time zone. 
 #' @return A date time object
 #' @seealso \code{\link{sql_datetime}}, \code{\link{ISOdatetime}} 
 #' and \code{\link{extract_date}}
 #' @examples
-#' data <- data.frame(Year = 2000, Month = 1:4, Day = 2)
+#' data <- data.frame(Year = 2000, Month = 1:12, Day = 2, 
+#'                    Hour = 12, Minute = 30, Second = 1)
 #' extract_datetime(data)
 #' extract_date(data)
 #' extract_time(data)
+#' 
+#' extract_datetime(data)
+#' extract_datetime(data, tz = "PDT")
+#' extract_datetime(data, tz = "PST8PDT")
+#' extract_datetime(data, tz = "UTC")
+#' extract_datetime(data, standardised_offset = -7)
 #' @export
 extract_datetime <- function (data, prefix = "", suffix = "",
-                              expand = c("Year", "Month", "Day", "Hour", "Minute", "Second"),
-                              tz = "") {
+                              expand = c("Year", "Month", "Day", "Hour",
+                                         "Minute", "Second"),
+                              tz = "PST", standardised_offset = -8) {
   
   assert_that(is.string(prefix) && noNA(prefix))
   assert_that(is.string(suffix) && noNA(suffix))
   assert_that(is.character(expand) && noNA(expand))
   assert_that(all(expand %in% c("Year", "Month", "Day", "Hour", "Minute", "Second")))
   assert_that(!any(duplicated(expand)))
+  assert_that(is.string(tz) && noNA(tz))
   
   values <- list()
   
@@ -43,12 +62,23 @@ extract_datetime <- function (data, prefix = "", suffix = "",
     }
   }
   
-  ISOdatetime(year = values[["Year"]], 
+  adjust <- switch(tz, "PDT" = -7, "PST" = -8, 0)
+  
+  if(adjust != 0)
+    tz <- "UTC"
+  
+  dt <- ISOdatetime(year = values[["Year"]], 
               month = values[["Month"]], 
               day = values[["Day"]],
               hour = values[["Hour"]],
               min = values[["Minute"]], 
               sec = values[["Second"]], tz = tz)
+  
+  if(adjust != 0) {
+    dt <- dt - hours(adjust)
+  }
+  
+  standardise_datetime(dt, standardised_offset = standardised_offset)
 }
 
 #' @title Extracts Date
@@ -96,16 +126,18 @@ extract_date <- function (data, prefix = "", suffix = "",
 #' @param expand character vector of column names where [prefix]expand[suffix]
 #' and expand must only be Hour, Minute and/or Second
 #' @param tz string of time zone
+#' @param standardised_offset an integer scalar indicating the time difference
+#' in hours between UTC and the desired time zone.
 #' @return A datetime object
 #' @seealso \link{sql_datetime} and \link{ISOdatetime}
 #' @export
 extract_time <- function (data, prefix = "", suffix = "",
                               expand = c("Hour", "Minute", "Second"),
-                              tz = "") {
+                              tz = "PST", standardised_offset = -8) {
   
   assert_that(is.character(expand) && noNA(expand))
   assert_that(all(expand %in% c("Hour", "Minute", "Second")))
   assert_that(!any(duplicated(expand)))
   
-  return (extract_datetime(data, prefix, suffix, expand, tz))
+  extract_datetime(data, prefix, suffix, expand, tz, standardised_offset)
 }
