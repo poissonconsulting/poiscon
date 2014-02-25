0c7fb67fc405ed8bd3fe6ba216680af4f9ffc340
Joe Thorley <joe@poissonconsulting.ca>
2014-02-25 14:43:10 -0800

    added flag dataset to extract dataset if required

--- R/load-plot.R
+++ R/load-plot.R
@@ -4,13 +4,15 @@
 #' Reads in ggplot object
 #' 
 #' @param name character scalar of the name of the plot (without extension).
-#' @param type the plot type i.e. figures (default) or analysis, data etc.
-#' @return a data.frame object
+#' @param type character scalar indicating the plot type i.e. figures (default) or analysis, data etc.
+#' @param dataset logical scalar indicating whether to return the data as opposed to the ggplot object
+#' @return the gggplot object or the plot data
 #' @export
-load_plot <- function (name = "plot", type = "figures") {
+load_plot <- function (name = "plot", type = "figures", dataset = FALSE) {
   
   assert_that(is.string(name))
   assert_that(is.string(type))
+  assert_that(is.flag(dataset) && noNA(dataset))
   
   name <- replace_ext(name, "rds")
   
@@ -18,6 +20,9 @@ load_plot <- function (name = "plot", type = "figures") {
   
   if (!file.exists (file))
     stop("the rds file associated with file ",replace_ext(file, "png")," does not exist")
+  
+  if(dataset)
+    return (readRDS(file)$plot$data)
     
   readRDS(file)$plot
 }
