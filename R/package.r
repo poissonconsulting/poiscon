7826035df68e88aff947e5c32457a69aa6273ec9
Joe Thorley <joe@poissonconsulting.ca>
2014-03-05 12:24:40 -0800

    added windowsFonts

--- R/package.r
+++ R/package.r
@@ -8,7 +8,7 @@
 #' @docType package
 #' @import assertthat devtools formatR ggplot2 grid jaggernaut hashFunction knitr lubridate markdown RODBC pingr reshape2 scales slidify stringr
 #' @importFrom plyr ddply rbind.fill .
-#' @importFrom grDevices quartzFonts
+#' @importFrom grDevices quartzFonts windowsFonts
 #' @importFrom tools file_ext file_path_sans_ext
 #' @name package-poiscon
 #' @aliases poiscon
