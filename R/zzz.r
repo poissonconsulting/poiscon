d046ab7adcba3e1f4416c6fed6dd9d8e500cb6f3
Joe Thorley <joe@poissonconsulting.ca>
2014-03-05 12:35:21 -0800

    moved set_fonts code in .onAttach

--- R/zzz.r
+++ R/zzz.r
@@ -1,8 +1,17 @@
 .onAttach <- function(libname, pkgname) {
   
+  suppressPackageStartupMessages(require(grDevices))
   reset_folders()
   
-#  set_fonts()
+  if (.Platform$OS.type == "unix") {
+    if (Sys.info()["sysname"] == "Darwin") {
+      quartzFonts(sans = quartzFont(rep("Arial", 4)))
+    } else {
+      stop("need to set font to Arial for linux operating systems")
+    }
+  } else {
+    windowsFonts(Arial = windowsFont("Arial"))
+  }
   
   theme_set(theme_Poisson())
   palette(palette_Poisson())
