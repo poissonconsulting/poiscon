8a5d8909024f0b1fb104da1866bcac5b899e7644
Joe Thorley <joe@poissonconsulting.ca>
2014-03-05 11:20:44 -0800

    removed install_hd and added onAttach theme_Poisson etc

---
+++ R/set-fonts.R
@@ -0,0 +1,11 @@
+set_fonts <- function () {
+  if (.Platform$OS.type == "unix") {
+    if (Sys.info()["sysname"] == "Darwin") {
+      quartzFonts(sans = quartzFont(rep("Arial", 4)))
+    } else {
+      stop("need to set font to Arial for linux operating systems")
+    }
+  } else {
+    windowsFonts(Arial = windowsFont("Arial"))
+  }
+}
--- R/zzz.r
+++ R/zzz.r
@@ -1,3 +1,11 @@
-.onAttach = function(lib, pkg) {
+.onAttach <- function(libname, pkgname) {
+  
   reset_folders()
+  
+  set_fonts()
+  
+  theme_set(theme_Poisson())
+  palette(palette_Poisson())
+  
+  invisible()
 }
