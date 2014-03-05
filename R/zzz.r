fa994ea5fdf341876dc659d4e9a4608d9361315c
Joe Thorley <joe@poissonconsulting.ca>
2014-03-05 12:47:39 -0800

    trying grDevices::

--- R/zzz.r
+++ R/zzz.r
@@ -1,20 +1,19 @@
 .onAttach <- function(libname, pkgname) {
   
-  suppressPackageStartupMessages(require(grDevices))
   reset_folders()
   
   if (.Platform$OS.type == "unix") {
     if (Sys.info()["sysname"] == "Darwin") {
-      quartzFonts(sans = quartzFont(rep("Arial", 4)))
+      grDevices::quartzFonts(sans = quartzFont(rep("Arial", 4)))
     } else {
       stop("need to set font to Arial for linux operating systems")
     }
   } else {
-    windowsFonts(Arial = windowsFont("Arial"))
+    grDevices::windowsFonts(Arial = windowsFont("Arial"))
   }
   
   theme_set(theme_Poisson())
-  palette(palette_Poisson())
+  grDevices::palette(palette_Poisson())
   
   invisible()
 }
