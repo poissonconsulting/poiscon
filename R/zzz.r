bf54e6dc89e6feddd5dd20990ade7e2e2c6d04e5
Joe Thorley <joe@poissonconsulting.ca>
2014-03-05 12:56:02 -0800

    commented out grDevices::

--- R/zzz.r
+++ R/zzz.r
@@ -2,18 +2,19 @@
   
   reset_folders()
   
-  if (.Platform$OS.type == "unix") {
-    if (Sys.info()["sysname"] == "Darwin") {
-      grDevices::quartzFonts(sans = grDevices::quartzFont(rep("Arial", 4)))
-    } else {
-      stop("need to set font to Arial for linux operating systems")
-    }
-  } else {
-    grDevices::windowsFonts(Arial = grDevices::windowsFont("Arial"))
-  }
+#   if (.Platform$OS.type == "unix") {
+#     if (Sys.info()["sysname"] == "Darwin") {
+#       grDevices::quartzFonts(sans = grDevices::quartzFont(rep("Arial", 4)))
+#     } else {
+#       stop("need to set font to Arial for linux operating systems")
+#     }
+#   } else {
+#     grDevices::windowsFonts(Arial = grDevices::windowsFont("Arial"))
+#   }
+#   grDevices::palette(palette_Poisson())
   
   theme_set(theme_Poisson())
-  grDevices::palette(palette_Poisson())
+
   
   invisible()
 }
