b9a1b9f94ad8f38a2a841a77e319f1ff4766397c
Joe Thorley <joe@poissonconsulting.ca>
2014-11-27 15:11:13 -0800

    loaded_value added digits and changes order of rowname and colname = "estimate" by default and name = "estimates" by default

--- R/load-value.R
+++ R/load-value.R
@@ -3,19 +3,21 @@
 #' @description 
 #' Reads in csv file from current table folder.
 #' 
-#' @param colname string of name of column containing value
 #' @param rowname string of name of row containing value
+#' @param colname string of name of column containing value
 #' @param folders string of folders containing table - separate folders with '/'
 #' @param name the name of the table (csv extension is not required)
 #' @param type the table type i.e. results (default) or analysis etc.
+#' @param digits count of number of significant digits
 #' @return a data.frame of the table
 #' @export
-load_value <- function(colname, rowname, folders = get_folders(), name = "table", type = "results") {
+load_value <- function(rowname, colname = "estimate", folders = get_folders(), name = "estimates", type = "results", digits = 6) {
   
   assert_that(is.string(colname))
   assert_that(is.string(rowname))
   assert_that(is.string(name))
   assert_that(is.string(type))
+  assert_that(is.count(digits))
   
   on.exit(set_folders(get_folders()))
   set_folders(folders)
@@ -28,5 +30,5 @@ load_value <- function(colname, rowname, folders = get_folders(), name = "table"
   assert_that(colname %in% colnames(table))
   assert_that(rowname %in% rownames(table))
   
-  table[rowname,colname,drop = TRUE]
+  signif(table[rowname,colname,drop = TRUE], digits = digits)
 } 
