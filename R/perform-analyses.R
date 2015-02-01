9b7d3f862e7e43dda08064b980dc27e1aad24e4a
Joe Thorley <joe@poissonconsulting.ca>
2015-02-01 12:19:02 -0800

    added trace plots to perform analyses

--- R/perform-analyses.R
+++ R/perform-analyses.R
@@ -24,6 +24,7 @@ perform_analyses <- function(models, ..., niters = 10^3, name = "data") {
     save_analysis(analysis)
     print(summary(analysis))
     save_tables(analysis)
+    save_plots(analysis)
   }
   
   if (nargs == 0) {
